4ca6814852356f9d4e1b85e7ad5bd078f471b498
Lawrence Casely-Hayford <lawrencecaselyhayford@gmail.com>
2019-02-17 19:50:11 +0000

    sample event broadcasting with laravel, nodejs,redis and socket

--- redis-server/server.js
+++ redis-server/server.js
@@ -3,12 +3,20 @@ var http = require("http").Server(app);
 var io = require("socket.io")(http);
 var Redis = require("ioredis");
 var redis = new Redis();
-redis.subscribe("new-user", function(err, count) {});
+
+//subscribe to messages on channel in laravel app
+redis.subscribe("new-user", function(err, count) {
+    console.log(count, " subcribed to this channel");
+});
+
 redis.on("message", function(channel, message) {
     console.log("Message Recieved: " + message);
+    //parse data and emit on channel 'new-user'
     message = JSON.parse(message);
     io.emit("new-user", message);
 });
+
+//listen to node app on server 3000
 http.listen(3000, function() {
     console.log("Listening on Port 3000");
 });
